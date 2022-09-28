5e91c30d5de746e647d6ceb79068919a856788e9
Nguyen-Quy <nguyenquy.kb94@gmail.com>
2022-09-28 11:12:16 +0700

    recommit

---
+++ 27-9/vue-task/src/main.js
@@ -0,0 +1,11 @@
+import { createApp } from 'vue'
+import * as Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+Vue.config.productionTip = false;
+
+Vue.prototype.$http = axios;
