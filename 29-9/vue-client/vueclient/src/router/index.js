f91834bd7fcd06b0e2826082a9e752b03bc428ec
Nguyen-Quy <nguyenquy.kb94@gmail.com>
2022-09-29 15:45:17 +0700

    test git

---
+++ 29-9/vue-client/vueclient/src/router/index.js
@@ -0,0 +1,23 @@
+import * as Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+window.Vue = Vue;
+
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: Home
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
---
+++ 29-9/vue-client/vueclient/src/views/Home.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="home">
+    Welcome to API test!
+    api data: {{post_data}}
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+  name: 'Home',
+  data() {
+    return {
+      'post_data':null
+    }
+  },
+
+  components: {
+    
+  },
+
+  computed: {
+    api_url() {
+      return 'http://localhost:8000'
+    }
+  },
+
+  methods: {
+    load_data() {
+      axios.get(this.api_url + '/api/posts').then((resp) => {
+        this.post_data = resp.data;
+      });
+    }
+  },
+
+  created() {
+    this.load_data();
+  }
+}
+</script>
